7958e439c86495aebe86924ba459df9a36fff41a
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-06 17:21:58 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,19 @@ class BaseModel:
         save
         to_dict
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+    
     def __str__(self):
         """
         Return: a string representation of the instance.
