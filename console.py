7642a87f1a056182d466770d1cb64cdecaafb28a
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-09 13:43:59 +0100

    Console 0.1

--- console.py
+++ console.py
@@ -26,19 +26,68 @@ Quit command to exit the program
         return True
     
     def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel
+        saves it (to the JSON file) and prints the id
+        """
         if not arg:
             print("** class name missing **")
-
         else:
             try:
-                cls = models.classes[arg]
+                cls = globals()[arg]
                 obj = cls()
                 models.storage.new(obj)
                 models.storage.save()
                 print(obj.id)
             except:
                 print("** class doesn't exist **")
-            
+
+
+    def do_show(self, arg):
+        """Usage: show <class> <id>
+        Prints the string representation of an instance based on the class name and id.
+        """
+        argl = arg.split()
+
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in globals() or not isinstance(globals()[argl[0]], type):
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(argl[0], argl[1])
+            instances = models.storage.all()
+            instance = instances.get(key)
+
+            if instance:
+                print(instance)
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        """
+        argl = arg.split()
+
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in globals() or not isinstance(globals()[argl[0]], type):
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(argl[0], argl[1])
+            instances = models.storage.all()
+            instance = instances.get(key)
+
+            if instance:
+                del instances[key]
+                models.storage.save()  # Save changes to the JSON file
+            else:
+                print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
