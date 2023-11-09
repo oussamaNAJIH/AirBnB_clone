5ff64916a8c7f4142d9f8248b66ecbd475a26c91
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-09 12:33:20 +0100

    Console 0.1

--- console.py
+++ console.py
@@ -3,7 +3,9 @@
 this module contains the entry point of the command interpreter
 """
 import cmd
-
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -22,7 +24,21 @@ Quit command to exit the program
 Quit command to exit the program
         """
         return True
+    
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
 
+        else:
+            try:
+                cls = models.classes[arg]
+                obj = cls()
+                models.storage.new(obj)
+                models.storage.save()
+                print(obj.id)
+            except:
+                print("** class doesn't exist **")
+            
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
